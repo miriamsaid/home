5ac322bd34e5ac4bf643ef04e58dcde9e55b777b
Miriam said <miriamsaid@Miriams-MacBook-Pro.local>
2020-10-18 21:06:07 +0100

    Updates

---
+++ precache-manifest.0400820dde4acbb00e836b47308b640a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a1de3d3b537c23770238664466515d9",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "88d7d44951adbdd01cfc",
+    "url": "/home/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "31d69260d482a940208b",
+    "url": "/home/static/css/main.9fc689ea.chunk.css"
+  },
+  {
+    "revision": "88d7d44951adbdd01cfc",
+    "url": "/home/static/js/2.8fa335fd.chunk.js"
+  },
+  {
+    "revision": "80fa05aac9f80c71b5f99ecec6ead10d",
+    "url": "/home/static/js/2.8fa335fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31d69260d482a940208b",
+    "url": "/home/static/js/main.0ed40774.chunk.js"
+  },
+  {
+    "revision": "5c07ecca0456e28e04f0",
+    "url": "/home/static/js/runtime-main.e084aacb.js"
+  },
+  {
+    "revision": "b8d695f657c8b4e5546d35d84528dea1",
+    "url": "/home/static/media/resume.b8d695f6.pdf"
+  }
+]);
