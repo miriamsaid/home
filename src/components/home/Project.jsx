c4fecb3731401c3fa79e9093c5882ea48e5d5075
Miriam said <miriamsaid@Miriams-MacBook-Pro.local>
2020-10-19 04:23:31 +0100

    added cards and removed previous project cards

--- src/components/home/Project.jsx
+++ src/components/home/Project.jsx
@@ -8,7 +8,6 @@ import {
   gitHubQuerry,
   projectsLength,
 } from "../../editable-stuff/configurations.json";
-
 const Project = () => {
   const [projectsArray, setProjectsArray] = useState([]);
 
@@ -34,19 +33,41 @@ const Project = () => {
   }, [handleRequest]);
 
   return (
-    <div id="projects" className="jumbotron jumbotron-fluid bg-transparent m-0">
-      {projectsArray.length && (
-        <div className="container container-fluid p-5">
-          <h1 className="display-4 pb-5">{projectHeading}</h1>
-          <div className="row">
-            {projectsArray.map((project) => (
-              <ProjectCard key={project.id} id={project.id} value={project} />
-            ))}
-          </div>
-        </div>
-      )}
+    <div className="container container-fluid p-5">
+    <h1 className="display-4 pb-5">{projectHeading}</h1>
+<main
+class="page-content">
+  <div class="card">
+    <div class="content">
+      <h2 class="title">Mountain View</h2>
+      <p class="copy">Check out all of these gorgeous mountain trips with beautiful views of, you guessed it, the mountains</p>
+      <button class="btn">View Trips</button>
     </div>
-  );
+  </div>
+  <div class="card">
+    <div class="content">
+      <h2 class="title">To The Beach</h2>
+      <p class="copy">Plan your next beach trip with these fabulous destinations</p>
+      <button class="btn">View Trips</button>
+    </div>
+  </div>
+  <div class="card">
+    <div class="content">
+      <h2 class="title">Desert Destinations</h2>
+      <p class="copy">It's the desert you've always dreamed of</p>
+      <button class="btn">Book Now</button>
+    </div>
+  </div>
+  <div class="card">
+    <div class="content">
+      <h2 class="title">Explore The Galaxy</h2>
+      <p class="copy">Seriously, straight up, just blast off into outer space today</p>
+      <button class="btn">Book Now</button>
+    </div>
+  </div>
+</main>
+</div>
+  )
 };
 
 export default Project;
