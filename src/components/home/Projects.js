df48333f602ac6d1e3b2bac3177b37cc3f696596
Miriam said <miriamsaid@Miriams-MacBook-Pro.local>
2020-10-20 05:28:33 +0100

    project add

--- src/components/home/Projects.js
+++ src/components/home/Projects.js
@@ -12,36 +12,45 @@ const Projects = () => {
   </section>
   
   <section>
-      <div class="parallax-one">
-        <h2>SOUTHERN CALIFORNIA</h2>
+      <div class="parallax-one min-vh-100 align-content-center align-items-center">
+        <h2>The FloBoat</h2>
+        <h3>a floating beach wheelchair.</h3>
       </div>
   </section>
   
   <section>
-    <div class="block">
-      <p><span class="first-character sc">I</span>n 1978, Brian Smith landed in Southern California with a bag of sheepskin boots and hope. He fell in love with the sheepskin experience and was convinced the world would one day share this love. The beaches of Southern California had long been an epicenter of a relaxed, casual lifestyle, a lifestyle that Brian felt was a perfect fit for his brand. So he founded the UGG brand, began selling his sheepskin boots and they were an immediate sensation. By the mid 1980's, the UGG brand became a symbol of relaxed southern California culture, gaining momentum through surf shops and other shops up and down the coast of California, from San Diego to Santa Cruz. UGG boots reached beyond the beach, popping up in big cities and small towns all over, and in every level of society. Girls wore their surfer boyfriend's pair of UGG boots like a letterman jacket. When winter came along, UGG boots were in ski shops and were seen in lodges from Mammoth to Aspen.</p>
+    <div class="block jumbotron jumbotron-fluid bgstyle min-vh-100 d-flex align-content-center align-items-center flex-wrap ">
+      <p><span class="first-character sc">W</span>heelchair users make up 1% of the global population. The increased social awareness of immobile individuals across the past century has driven engineers into creating the adaptable wheelchair design seen in the present day. Technology advances have brought about the increased integration of wheelchair users into society by providing support; in completing daily tasks and in participating in specific leisure activities, however, the majority of water activities remain inaccessible.
+Therefore, a design opportunity was recognised which subsequently fuelled this study in creating the FloBoat, a wheelchair that provides support to individuals when in the water and on the beach. The FloBoat journeyed through a rigorous design approach, fulfilling the necessary design requirements to operate with maximum functionality and effectiveness. Throughout the design analysis, investigations were completed on design tasks such as material selection, hydrostatics and stability.
+Finite Element Analysis alongside a material selection database, GRANTA EduPack, deduced component materials and a suitable fibre layup for the CFRP-based chair body.</p>
       <p class="line-break margin-top-10"></p>
-      <p class="margin-top-10">The UGG brand began to symbolize those who embraced sport and a relaxed, active lifestyle. More than that, an emotional connection and a true feeling of love began to grow for UGG boots, just as Brian had envisioned. People didn't just like wearing UGG boots, they fell in love with them and literally could not take them off. By the end of the 90's, celebrities and those in the fashion world took notice of the UGG brand. A cultural shift occurred as well - people were embracing, and feeling empowered, by living a more casual lifestyle and UGG became one of the symbols of this lifestyle. By 2000, a love that began on the beaches had become an icon of casual style. It was at this time that the love for UGG grew in the east, over the Rockies and in Chicago. In 2000, UGG Sheepskin boots were first featured on Oprah's Favorite Things® and Oprah emphatically declared that she "LOOOOOVES her UGG boots." From that point on, the world began to notice.</p>
+      <p class="margin-top-10">The FloBoat demonstrated the ability to float and achieved an average draft of 659mm at 26.4° from the base. Stability in water was additionally attained when the metacentric height of the final design was calculated to be 0.0014m indicating initial device stability. The final design was created as a Computer-Aided Design model. Manufacturing and business plans were subsequently compiled to assess the financial feasibility of the FloBoat. The pricing model achieved a healthy gross margin averaging to 67.8% leading to 5-year projections that indicate a breakeven point in the first operating year after 146 sales. This design study has fashioned an economical device that can effectively support immobile individuals in enjoying the water whilst maintaining health, safety and durability standards.</p>
     </div>
   </section>
   
   <section>
-    <div class="parallax-two">
-      <h2>NEW YORK</h2>
+    <div class="parallax-two min-vh-100 align-content-center align-items-center">
+      <h4 >Simple Sorting</h4>
+      <h5>an automated recyling sorting bin.</h5>
     </div>
   </section>
   
   <section>
-    <div class="block">
-      <p><span class="first-character ny">B</span>reaking into the New York fashion world is no easy task. But by the early 2000's, UGG Australia began to take it by storm. The evolution of UGG from a brand that made sheepskin boots, slippers, clogs and sandals for an active, outdoor lifestyle to a brand that was now being touted as a symbol of a stylish, casual and luxurious lifestyle was swift. Much of this was due to a brand repositioning effort that transformed UGG into a high-end luxury footwear maker. As a fashion brand, UGG advertisements now graced the pages of Vogue Magazine as well as other fashion books. In the mid 2000's, the desire for premium casual fashion was popping up all over the world and UGG was now perfectly aligned with this movement.</p>
+    <div class="block jumbotron jumbotron-fluid bgstyle min-vh-100 d-flex align-content-center align-items-center flex-wrap ">
+      <p><span class="first-character ny">H</span>Half of the plastic that has ever been generated has been produced in the last 13 years. If these recent trends continue, it is estimated that 26 billion tons of plastic will be produced in the next 30 years. To reduce these environmental impacts the radical way of thinking needs to be changed, by optimising the use of this material and increasing its life cycle through reuse and recycling [1]. Statistics show that 15% of waste that arrive at recycling plants are non-recyclable or highly contaminated [2]. Thus, making it difficult for waste management facilities to be accurate during the recycling process, which results in wasting time, money, and labor.
+Promoting proper waste management, by combining education models and appropriate technologies, is a key contribution to sustainable development. The aim of the project is to overcome these difficulties by developing a self-sorting machine with the implementation of artificial intelligence. The goal is to separate recyclables from non-recyclables, thus preventing the presences of unwanted waste at recycling plants beforehand.
+Once the purpose and design of the product was chosen, a questionnaire was conducted for primary research regarding the potential market of the automatic recycling bin. The research concluded that Universities are the best potential market for the sorting machine as on average Universities in the UK spend £250,000 a year on waste management and a potential market size of £1.08 million was estimated.
+Simple Sorting was designed, developed and tested by group Pangea according to the Universities needs and demands by incorporating Google Cloud Vision API. This was achieved by developing design evaluation criteria and costumer competitive assessment, which concluded that Simple Sorting would be designed to be eco- friendly and highly affordable compared to existing products on the market.</p>
       <p class="line-break margin-top-10"></p>
-      <p class="margin-top-10">Fueled by celebrities from coast to coast wearing UGG boots and slippers on their downtime, an entirely new era of fashion was carved out. As a result, the desire and love for UGG increased as people wanted to go deeper into this relaxed UGG experience. UGG began offering numerous color and style variations on their sheepskin boots and slippers. Cold weather boots for women and men and leather casuals were added with great success. What started as a niche item, UGG sheepskin boots were now a must-have staple in everyone's wardrobe. More UGG collections followed, showcasing everything from knit boots to sneakers to wedges, all the while maintaining that luxurious feel UGG is known for all over the world. UGG products were now seen on runways and in fashion shoots from coast to coast. Before long, the love spread even further.</p>
+      <p class="margin-top-10">To satisfy these requirements Finite Element Analysis was used to determine which material was most suitable for this design and to understand the service life of the product, which came to 10.3 years.
+Group Pangea developed a prototype which demonstrated the ability of Simple Sorting to sort different types of waste according to recyclable or non-recyclable. This was achieved with the Google AIY Vision Kit which operates by means of image recognition. An evaluation plan was developed to understand if the final prototype would have met the design criteria establish at the beginning of the project.
+Finally, a Manufacturing and Business plan were developed for Simple Sorting, concluding that the manufacturing location would be established in China, the market location would be focused initially at UK universities, and gradually expand to Europe. The financial model estimated that profit is made after the second year of operation by selling bins at a feasible price of £380 with a monthly subscription fee of £15.</p>
     </div>
   </section>
   
   <section>
     <div class="parallax-three">
-      <h2>ENCHANTED FOREST</h2>
+      <h2>The Finite Element Analysis of a Tidal Turbine</h2>
     </div>
   </section>
   
@@ -52,7 +61,20 @@ const Projects = () => {
       <p class="margin-top-10">In 2011, UGG will go back to its roots and focus on bringing the active men that brought the brand to life back with new styles allowing them to love the brand again as well. Partnering with Super Bowl champion and NFL MVP Tom Brady, UGG will invite even more men to feel the love the rest of the world knows so well. UGG will also step into the world of high fashion with UGG Collection. The UGG Collection fuses the timeless craft of Italian shoemaking with the reliable magic of sheepskin, bringing the luxurious feel of UGG to high end fashion. As the love for UGG continues to spread across the world, we have continued to offer new and unexpected ways to experience the brand. The UGG journey continues on and the love for UGG continues to spread.</p>
     </div>
   </section>
-    
+
+  <section>
+    <div class="parallax-four">
+      <h2>The Trike</h2>
+    </div>
+  </section>
+  
+  <section>
+    <div class="block">
+      <p><span class="first-character atw">W</span>hen the New York fashion community notices your brand, the world soon follows. The widespread love for UGG extended to Europe in the mid-2000's along with the stylish casual movement and demand for premium casual fashion. UGG boots and shoes were now seen walking the streets of London, Paris and Amsterdam with regularity. To meet the rising demand from new fans, UGG opened flagship stores in the UK and an additional location in Moscow. As the love spread farther East, concept stores were opened in Beijing, Shanghai and Tokyo. UGG Australia is now an international brand that is loved by all. This love is a result of a magical combination of the amazing functional benefits of sheepskin and the heightened emotional feeling you get when you slip them on your feet. In short, you just feel better all over when you wear UGG boots, slippers, and shoes.</p>
+      <p class="line-break margin-top-10"></p>
+      <p class="margin-top-10">In 2011, UGG will go back to its roots and focus on bringing the active men that brought the brand to life back with new styles allowing them to love the brand again as well. Partnering with Super Bowl champion and NFL MVP Tom Brady, UGG will invite even more men to feel the love the rest of the world knows so well. UGG will also step into the world of high fashion with UGG Collection. The UGG Collection fuses the timeless craft of Italian shoemaking with the reliable magic of sheepskin, bringing the luxurious feel of UGG to high end fashion. As the love for UGG continues to spread across the world, we have continued to offer new and unexpected ways to experience the brand. The UGG journey continues on and the love for UGG continues to spread.</p>
+    </div>
+  </section>
   </div>
   );
 };
